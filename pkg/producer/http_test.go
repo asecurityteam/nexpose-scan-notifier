bd30a0d65905719b9121221e05147a46af35f54a
Ben Krebsbach <bkrebsbach@atlassian.com>
2019-06-04 11:07:58 -0500

    Check HTTP producer response status code

--- pkg/producer/http_test.go
+++ pkg/producer/http_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -15,48 +16,82 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestProduceSuccess(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockRT := NewMockRoundTripper(ctrl)
-
-	scan := domain.CompletedScan{
-		ScanID: "1",
-		SiteID: "2",
-	}
-
-	respJSON, _ := json.Marshal(scan)
-	respReader := bytes.NewReader(respJSON)
-	mockRT.EXPECT().RoundTrip(gomock.Any()).Return(&http.Response{
-		Body:       ioutil.NopCloser(respReader),
-		StatusCode: http.StatusOK,
-	}, nil)
-
-	endpoint, _ := url.Parse("http://localhost")
-	producer := &HTTP{
-		Client:   &http.Client{Transport: mockRT},
-		Endpoint: endpoint,
-	}
-	err := producer.Produce(context.Background(), scan)
-	require.Nil(t, err)
+type errReader struct {
+	Error error
 }
 
-func TestProduceError(t *testing.T) {
+func (r *errReader) Read(_ []byte) (int, error) {
+	return 0, r.Error
+}
+
+func TestHTTP_Produce(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockRT := NewMockRoundTripper(ctrl)
-
-	mockRT.EXPECT().RoundTrip(gomock.Any()).Return(nil, errors.New("HTTPError"))
 
 	scan := domain.CompletedScan{
 		ScanID: "1",
 		SiteID: "2",
 	}
+	respJSON, _ := json.Marshal(scan)
+	respReader := bytes.NewReader(respJSON)
 	endpoint, _ := url.Parse("http://localhost")
 	producer := &HTTP{
 		Client:   &http.Client{Transport: mockRT},
 		Endpoint: endpoint,
 	}
-	err := producer.Produce(context.Background(), scan)
-	require.NotNil(t, err)
+
+	tests := []struct {
+		name        string
+		response    *http.Response
+		responseErr error
+		expectErr   bool
+	}{
+		{
+			name: "success",
+			response: &http.Response{
+				Body:       ioutil.NopCloser(respReader),
+				StatusCode: http.StatusOK,
+			},
+			responseErr: nil,
+			expectErr:   false,
+		},
+		{
+			name:        "request error",
+			response:    nil,
+			responseErr: errors.New("HTTPError"),
+			expectErr:   true,
+		},
+		{
+			name: "non 200 status code",
+			response: &http.Response{
+				Body:       ioutil.NopCloser(respReader),
+				StatusCode: http.StatusNotFound,
+			},
+			responseErr: nil,
+			expectErr:   true,
+		},
+		{
+			name: "io read error",
+			response: &http.Response{
+				Body:       ioutil.NopCloser(&errReader{Error: fmt.Errorf("io read error")}),
+				StatusCode: http.StatusOK,
+			},
+			responseErr: nil,
+			expectErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRT.EXPECT().RoundTrip(gomock.Any()).Return(tt.response, tt.responseErr)
+			err := producer.Produce(context.Background(), scan)
+			if tt.expectErr {
+				require.Error(t, err)
+				return
+			}
+			require.Nil(t, err)
+		})
+	}
+
 }
